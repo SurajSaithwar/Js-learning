c078fa895747a0963ee60b11c37a16ef8b355fe2
Suraj Saithwar <saithwarsuraj@gmail.com>
2024-08-23 15:51:09 +0000

    Container configure

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=1234
+let accountEmail="Saithwar@gmail.com"
+var accountPassword="1234"
+accountCity="Bangalore"
+let acccountState;
+
+// accountId=3 npt allowed
+
+accountEmail="aaabbbb.com"
+accountPassword="1111"
+accountCity="pune"
+
+console.log(accountId)
+//node 
+console.table([accountId,accountEmail,accountPassword,accountCity,])
